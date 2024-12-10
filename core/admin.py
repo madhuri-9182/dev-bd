e490e46a94c0236021eae2e267c10e006a71775c
Sumit Dey <sumitdeyodisha@gmail.com>
2024-12-10 14:08:30 +0530

    Added permission and content type models in core admin.py field for admin user

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 from .models import User
 
 
@@ -7,3 +9,6 @@ class UserAdmin(admin.ModelAdmin):
 
 
 admin.site.register(User, UserAdmin)
+
+admin.site.register(Permission)
+admin.site.register(ContentType)
