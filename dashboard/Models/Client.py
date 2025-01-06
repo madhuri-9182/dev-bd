b323468636abe9a713351ce0256530fb87a2264a
Sumit Dey <sumitdeyodisha@gmail.com>
2025-01-06 19:29:42 +0530

    1. Added ClientUser, Job, Candidate and Interview model 2. Added one common utility class for created, updated and archived in Modelutils.py file

--- dashboard/Models/Client.py
+++ dashboard/Models/Client.py
@@ -1,40 +1,143 @@
 from organizations.models import Organization
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
 from phonenumber_field.modelfields import PhoneNumberField
-from core.models import User
-from hiringdogbackend.ModelUtils import SoftDelete
+from core.models import User, Role
+from hiringdogbackend.ModelUtils import SoftDelete, CreateUpdateDateTimeAndArchivedField
+from .Internal import InternalInterviewer
 
 
-class ClientUser(models.Model):
+class ClientUser(models.Model, CreateUpdateDateTimeAndArchivedField):
+    REASON_FOR_ARCHIVED_CHOICES = (
+        ("PF", "Postition Filled"),
+        ("POH", "Position On Hold"),
+        ("OTH", "Other"),
+    )
     objects = SoftDelete()
-    user = models.ForeignKey(
-        User,
+    Organization = models.ForeignKey(
+        Organization, on_delete=models.CASCADE, related_name="clientuser", blank=True
+    )
+    User = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="clientuser", blank=True
+    )
+    name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(max_length=100, unique=True, blank=True)
+    designation = models.CharField(max_length=100, blank=True)
+    role = models.CharField(
+        max_length=15,
+        choices=Role.choices,
+        blank=True,
+        verbose_name="user_type",
+    )
+    reason_for_archived = models.CharField(
+        max_length=15, choices=REASON_FOR_ARCHIVED_CHOICES, blank=True, null=True
+    )
+
+
+class Job(models.Model, CreateUpdateDateTimeAndArchivedField):
+    objects = SoftDelete()
+    object_all = models.Manager()
+    client = models.ForeignKey(
+        ClientUser, on_delete=models.CASCADE, related_name="recruiter", blank=True
+    )
+    name = models.CharField(max_length=100, blank=True)
+    job_id = models.CharField(max_length=100, blank=True, null=True)
+    hiring_manager = models.ForeignKey(
+        ClientUser,
         on_delete=models.CASCADE,
-        related_name="clientuser",
-        null=True,
+        related_name="hiringmanager",
         blank=True,
     )
-    name = models.CharField(max_length=100, null=True, blank=True)
-    email = models.EmailField(
-        max_length=100,
-        unique=True,
-        null=True,
+    total_positions = models.PositiveSmallIntegerField(default=0)
+    job_description_file = models.FileField(upload_to="job_descriptions")
+    mandatory_skills = models.TextField(blank=True)
+    interview_time = models.CharField(max_length=50)
+    other_details = models.JSONField(default=dict, blank=True)
+
+
+class Candidate(models.Model, CreateUpdateDateTimeAndArchivedField):
+    STATUS_CHOICES = (
+        ("REC", "Recommended"),
+        ("NREC", "Not Recommended"),
+        ("SCH", "Schedule"),
+        ("NSCH", "Not Schedule"),
+    )
+    REASON_FOR_DROPPING_CHOICES = (
+        ("CNI", "Candidate Not Interested"),
+        ("CNA", "Candidate Not Available"),
+        ("CNR", "Candidate Not Respond"),
+        ("OTH", "Others"),
+    )
+    objects = SoftDelete()
+    object_all = models.Manager()
+    name = models.CharField(max_length=100, blank=True)
+    organization = models.ForeignKey(
+        Organization, on_delete=models.CASCADE, related_name="candidate"
+    )
+    year = models.PositiveSmallIntegerField(
+        default=0, help_text="candidate experience total year"
+    )
+    month = models.PositiveBigIntegerField(
+        default=0, help_text="candidate experience total month"
+    )
+    phone = PhoneNumberField(region="IN", blank=True)
+    email = models.EmailField(max_length=255, blank=True)
+    company = models.CharField(max_length=100, blank=True)
+    designation = models.CharField(max_length=100, blank=True)
+    source = models.CharField(
+        max_length=100, blank=True, help_text="From Which side this candidate is ?"
+    )
+    cv = models.FileField(upload_to="candidate_cvs")
+    remark = models.TextField(max_length=255, blank=True, null=True)
+    specialization = models.CharField(max_length=100, blank=True)
+    status = models.CharField(
+        max_length=15,
+        choices=STATUS_CHOICES,
+        blank=True,
+        default="NSCH",
+        help_text="candidate interview status",
+    )
+    reason_for_dropping = models.CharField(
+        max_length=100, choices=REASON_FOR_DROPPING_CHOICES, blank=True
+    )
+    score = models.PositiveSmallIntegerField(default=0)
+    total_score = models.PositiveSmallIntegerField(default=0)
+
+
+class Interview(models.Model, CreateUpdateDateTimeAndArchivedField):
+    candidate = models.ForeignKey(
+        Candidate, on_delete=models.DO_NOTHING, related_name="interview", blank=True
+    )
+    interivewer = models.ForeignKey(
+        InternalInterviewer,
+        on_delete=models.DO_NOTHING,
+        related_name="interviews",
         blank=True,
     )
-    user_type = models.CharField(max_length=100, null=True, blank=True)
-    JOB_ASSIGNED_CHOICES = (
-        ("sde3", "SDE III"),
-        ("pe", "PE"),
-        ("sde2", "SDE II"),
-        ("devops1", "DevOps I"),
-        ("em", "EM"),
-        ("sdet2", "SDET II"),
-        ("sdet1", "SDET I"),
+    status = models.CharField(
+        max_length=15,
+        choices=Candidate.STATUS_CHOICES,
+        blank=True,
+        help_text="Interview status",
     )
-    job_assigned = models.CharField(
-        max_length=10, choices=JOB_ASSIGNED_CHOICES, null=True, blank=True
+    date = models.DateTimeField(help_text="Scheduled interview date and time")
+    reschedule = models.BooleanField(
+        default=False, help_text="Indicates if the interview has been rescheduled"
     )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    archived = models.BooleanField(default=False)
+    recording = models.FileField(
+        upload_to="interview_recordings",
+        blank=True,
+        null=True,
+        help_text="Interview recording file",
+    )
+    feedback = models.TextField(
+        blank=True, null=True, help_text="Feedback for the candidate"
+    )
+    score = models.PositiveSmallIntegerField(default=0)
+    total_score = models.PositiveSmallIntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.candidate.status = self.status
+        self.candidate.score = self.score
+        self.candidate.total_score = self.total_score
+        self.candidate.save()
--- hiringdogbackend/ModelUtils.py
+++ hiringdogbackend/ModelUtils.py
@@ -4,3 +4,9 @@ from django.db import models
 class SoftDelete(models.Manager):
     def get_queryset(self) -> models.QuerySet:
         return super().get_queryset().filter(archived=False)
+
+
+class CreateUpdateDateTimeAndArchivedField:
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    archived = models.BooleanField(default=False)
