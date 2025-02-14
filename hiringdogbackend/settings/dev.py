4de42de1f0bed23d0af31ccc91f0e2fcabcb65c9
Sumit Dey <sumitdeyodisha@gmail.com>
2025-02-14 13:24:08 +0530

    Remove generics regex from dev.py to base.py and added debug value dynamically

--- hiringdogbackend/settings/dev.py
+++ hiringdogbackend/settings/dev.py
@@ -1,6 +1,6 @@
 from .base import *
 
-DEBUG = True
+DEBUG = sys.argv[1] == "runserver"
 
 SECRET_KEY = "django-insecure-pn-#@uf@1!0lm!7p27d5^_)2)yw=6joel7qklh)8l(!p4fe_&_"
 
@@ -17,9 +17,3 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-
-REGEX_GSTIN_BASIC = r"^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{15}$"
-REGEX_GSTIN = "^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$"
-REGEX_PAN = r"^[A-Za-z]{5}[0-9]{4}[A-Za-z]$"
-REGEX_PAN_BASIC = r"^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{10}$"
