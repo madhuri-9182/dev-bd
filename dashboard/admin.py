f5ff21d855bc37f6bc96cba49dc7e2ecd613da77
Ubuntu <ubuntu@ip-172-31-15-152.ap-south-1.compute.internal>
2025-05-01 17:39:21 +0530

    Added Admin MOdel for BillingRecord, InterviewFeedback and Interview model in admin.py

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -16,9 +16,45 @@ from .models import (
     Interview,
     InterviewerAvailability,
     InterviewFeedback,
+    BillingRecord,
 )
 
-admin.site.register(Interview)
+@admin.register(Interview)
+class InterviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'get_candidate_name',
+        'get_interviewer_name',
+        'client_amount',
+        'interviewer_amount',
+        'get_organization_name',
+        'created_at',
+        "scheduled_time",
+        "status",
+    )
+    list_filter = ('interviewer__name', 'candidate__organization__internal_client__name')
+    search_fields = ('candidate__name', 'interviewer__name', 'candidate__organization__internal_client__name')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related(
+            'candidate',
+            'candidate__organization',
+            'candidate__organization__internal_client',
+            'interviewer'
+        )
+
+    def get_candidate_name(self, obj):
+        return obj.candidate.name if hasattr(obj.candidate, 'name') else None
+    get_candidate_name.short_description = 'Candidate'
+
+    def get_interviewer_name(self, obj):
+        return obj.interviewer.name if hasattr(obj.interviewer, 'name') else None
+    get_interviewer_name.short_description = 'Interviewer'
+
+    def get_organization_name(self, obj):
+        return obj.candidate.organization.internal_client.name
+    get_organization_name.short_description = 'Organization'
 
 
 @admin.register(InternalInterviewer)
@@ -87,4 +123,49 @@ class CandidateAdmin(admin.ModelAdmin):
 
 
 admin.site.register(InterviewerAvailability)
-admin.site.register(InterviewFeedback)
+
+
+@admin.register(InterviewFeedback)
+class InterviewFeedbackAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'get_interview_name',
+        'overall_remark',
+        'overall_score',
+        'is_submitted'
+    )
+    list_filter = ('is_submitted',)
+    search_fields = ('interview__candidate__name', 'interview__interviewer__name')
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related('interview', 'interview__candidate', 'interview__interviewer')
+
+    def get_interview_name(self, obj):
+        return f"{obj.interview.candidate.name} - {obj.interview.interviewer.name}"
+    get_interview_name.short_description = "Interview"
+
+
+@admin.register(BillingRecord)
+class BillingRecordAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'amount_due',
+        "due_date",
+        'get_client_name',
+        'get_interviewer_name',
+    )
+    list_filter = ('client', 'interviewer')
+    search_fields = ('client__name', 'interviewer__name')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related('client', 'interviewer')
+
+    def get_client_name(self, obj):
+        return obj.client.name if hasattr(obj.client, 'name') else None
+    get_client_name.short_description = 'Client'
+
+    def get_interviewer_name(self, obj):
+        return obj.interviewer.name if hasattr(obj.interviewer, 'name') else None
+    get_interviewer_name.short_description = 'Interviewer'
